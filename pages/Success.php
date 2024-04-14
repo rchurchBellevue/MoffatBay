8dbb79b71cdc7c83ecf34ab45d4e12045d2c38c0
Ryan Church <rchurch@my365.bellevue.edu>
2024-04-13 21:57:38 -0400

    Many changes to connect backend to front end
    Created LoginSuccess and RegistrationSuccess pages.
    
    Removed placeholder values
    replaced inline javascript with a reference to the JS file
    added validation for input forms

---
+++ pages/Success.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Moffat Bay Marina</title>
+    <script type="text/javascript" src="../js/script.js"></script>
+</head>
+<body>
+    <!-- HEADER -->
+    <div class="header">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Reservations</li>
+            </ul>
+            <ul>
+                <li>Login</li>
+            </ul>
+        </nav>
+        <header>
+            <h1>Moffat Bay Marina</h1>
+        </header>
+    </div>
+    <!-- MAIN CONTENT -->
+    <div>
+        <h1>Successful Registration!</h1>
+        <h4>Registration is complete and you can now login and book a slip</h3>
+    </div>
+</body>
+</html>
---
+++ php/database.php
@@ -0,0 +1,41 @@
+<?php
+
+Class Database {
+
+private  static $server = "mysql:host=localhost;dbname=marinaDB";
+private  static $user = "marinaAdmin";
+private  static $pass = "Moff@Bay1#";
+
+private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+
+private function __construct(){}
+private static $conn;
+
+public static function getConn(){
+    if(!isset(self::$conn)){
+         try{
+       self::$conn=new PDO(self::$server,self::$user,self::$pass);
+
+          }
+          catch(PDOException $e)
+          {
+             $error=$e->getMessage(); 
+             exit();
+          }
+    }
+      return self::$conn; 
+   }
+
+
+ 
+
+
+public static function closeConnection() {
+   	$conn = null;
+	}
+
+
+        
+}
+
+?>
